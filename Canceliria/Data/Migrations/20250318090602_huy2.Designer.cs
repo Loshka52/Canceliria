a8919bde02d2e4b9f59d99e2c68d2ee3a25c0607
10241871 <10241871@D204PC025>
2025-03-18 12:28:56 +0300

    aaakujgkjhuk

--- Canceliria/Data/Migrations/20250318090602_huy2.Designer.cs
+++ Canceliria/Data/Migrations/20250318090602_huy2.Designer.cs
@@ -4,6 +4,7 @@ using Canceliria.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Canceliria.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250318090602_huy2")]
+    partial class huy2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
